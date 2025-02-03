5808df1f9368cb4c67de9716ef3317fad6fe35d4
Inquisitiveshri34 <diyashrivastava34@gmail.com>
2025-02-03 11:19:26 +0530

    committed

--- config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-// Connect to MongoDB
-const connectDb = async () => {
-    try {
-        await mongoose.connect(MONGO_URL)
-    }
-}
--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const mongoose = require("mongoose")
+app.use(express.json())
+
+app.post("/signup", async (req,res)=>{
+    const {username, email, password} = req.body;
+    if (username != ""){ 
+        if (email != ""){
+            if (password.length() > 8 && password.length() < 16){
+                res.send("User Added")
+            } else {
+                console.error("Password expectations failed")
+            }
+        } else { 
+            console.error("Email is required")
+        }
+    } else{
+    console.error("Username is required")
+}
+})
+
 
 app.get("/", (req,res)=>{
     res.send("Root is working!")
